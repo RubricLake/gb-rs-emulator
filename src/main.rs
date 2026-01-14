842c5cd75253be00a2c262464eb860d53c410e71
Ethan <rubriclake2@gmail.com>
2026-01-14 13:30:40 -0600

    error handling and file usage

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,27 @@ fn get_cartridge_path() -> PathBuf {
 }
 
 fn main() {
-    // Debug Path For Now
     let cartridge_path = Path::new(env!("CARGO_MANIFEST_DIR"))
         .join("rom")
         .join("Tetris.gb");
 
+    let boot_path = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("rom")
+        .join("dmg_boot.bin");
+
     let mut emu = Emulator::new();
-    
-    // Load Cartridge From .gb File
-    emu.load_cartridge(cartridge_path);
+
+    // Load Boot ROM from file
+    match emu.load_boot_rom(&boot_path) {
+        Ok(_) => log_println!("Successfully loaded boot ROM"),
+        Err(error) => log_println!("Failed to load boot ROM: {}", error),
+    }
+
+    // Load Cartridge ROM from file
+    match emu.insert_cartridge(&cartridge_path) {
+        Ok(_) => log_println!("Successfully inserted cartridge"),
+        Err(error) => log_println!("Failed to insert cartridge: {}", error),
+    }
 
     // Halt Bug Present on Gameboy CPU
     emu.set_halt_bug();
